5c73874931ecebfd884673ccdc88b8a09c3f1470
HP <qwe@>
2024-11-26 19:36:30 +0300

    123

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,14 @@
 import static java.lang.Math.PI;
 
 public class Main {
-    // Метод для вычисления производной функции y = sin(x)
     public static double derivative(double x) {
         double h = 1e-10; // малое значение для приближения
         return (Math.sin(x + h) - Math.sin(x - h)) / (2 * h);
     }
     public static void main(String[] args) {
-        double x = PI / 2;
+        double x = PI/6;
         double derivativeValue = derivative(x);
         System.out.println("Производная функции y = sin(x) в x = " + x + " равна: " + derivativeValue);
+        System.out.println("Производная функции y = sin(x) в x = " + x + " равна: " + Math.cos(x));
     }
 }
