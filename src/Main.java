de88c58235c62c33cc5b34a82bc46048ba6875c2
HP <qwe@>
2024-12-08 22:33:14 +0300

    derivate2

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,19 @@
 import static java.lang.Math.PI;
 
 public class Main {
-    public static double derivative(double x) {
-        double h = 1e-10; // малое значение для приближения
-        return (Math.sin(x + h) - Math.sin(x - h)) / (2 * h);
+    public static void derivative(double x) {
+        double h = 1e-10;
+        double[] r = new double[7];
+        int j = 0;
+        for (double i = x; i < PI/2; i = i + PI/6 ) {
+            r[j] =  (Math.sin(i + h) - Math.sin(i - h)) / (2 * h);
+            System.out.println("Производная функции y = sin(x) в x = " + i + " равна: " + r[j]);
+//            System.out.println("Производная функции y = sin(x) в x == " + i + " равна: " + Math.cos(i));
+            j++;
+        }
     }
-    public static void main(String[] args) {
-        double x = PI/6;
-        double derivativeValue = derivative(x);
-        System.out.println("Производная функции y = sin(x) в x = " + x + " равна: " + derivativeValue);
-
-        System.out.println("Производная функции y = sin(x) в x = " + x + " равна: " + Math.cos(x));
+    public static void main(String[] args){
+        double x = PI / 6;
+        derivative(x);
     }
 }
