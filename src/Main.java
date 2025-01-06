01ce57b34c875e8f4fb2506c73e99f10334c8c03
HP <qwe@>
2025-01-06 15:53:55 +0300

    chordMethod

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,13 @@ public class Main {
     public static double f(double x) {
         return 2 * x * x - 1 / (1 + x) - 16;
     }
-    // Метод хорд
+
     public static double findRoot(double a, double b, double epsilon) {
         double c;
         c = a - f(a) * (b - a) / (f(b) - f(a));
         int iteration = 0;
         System.out.printf("%-10s%-15s%-15s%-15s%-15s%n", "Итерация", "a", "b", "c", "f(c)");
-        while (Math.abs(b - a) > epsilon&&Math.abs(f(c))>epsilon) {
+        while (Math.abs(b - a) > epsilon && Math.abs(f(c))>epsilon) {
             iteration++;
             c = a - f(a) * (b - a) / (f(b) - f(a));
             System.out.printf("%-10d%-15f%-15f%-15f%-15f%n", iteration, a, b, c, f(c));
@@ -20,13 +20,13 @@ public class Main {
                 a = c;
             }
         }
-        return c; // Возвращаем среднюю точку интервала
+        return c;
     }
 
     public static void main(String[] args) {
         double a = 2;
         double b = 4;
-        double epsilon = 0.01;
+        double epsilon = 0.001;
         double root = findRoot(a, b, epsilon);
         System.out.printf("Найденный корень: %.6f%n", root);
     }
